606fca26215cfe73a1491d01afcdf08077432cc9
Jashwanth <jashwanthreddy2021@gmail.com>
2025-04-18 21:03:55 +0530

    Updated Hook file

--- src/test/java/steps/MyHooks.java
+++ src/test/java/steps/MyHooks.java
@@ -16,7 +16,7 @@ public class MyHooks {
 
     @Before
     public void setup() throws IOException {
-        System.out.println("WebDriver session has started!!!");
+        System.out.println("WebDriver session has started!!!+22");
         Properties prop = ConfigReader.intiliaseProperties();
         DriverFactory.initializeBrowser(prop.getProperty("browser"));
         driver = DriverFactory.getDriver();
